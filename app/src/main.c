4383609eaca7c682fdb80900056a95934954a6bd
Rijesh <trijesh92@gmail.com>
2026-09-01 22:43:59 +0200

    fix: threads with different priorities

--- app/src/main.c
+++ app/src/main.c
@@ -5,27 +5,54 @@ LOG_MODULE_REGISTER(demo, LOG_LEVEL_DBG);
 
 #define STACK_SIZE 1024
 
-#define PRIO_A 5
-#define PRIO_B 5
+#define PRIO_COP -1
+#define PRIO_LOW 7
+#define PRIO_MED 5
+#define PRIO_HIGH 3
 
-void thread_a_fn(void *p1, void *p2, void *p3)
+void t_cop_fn(void *p1, void *p2, void *p3)
 {
     while (1) {
-        k_msleep(200);
+        for(uint8_t i=0;i<5;i++)
+        {
+            LOG_INF("T_COP running %d", i);
+        }
+        k_yield();
     }
 }
 
-void thread_b_fn(void *p1, void *p2, void *p3)
+void t_low_fn(void *p1, void *p2, void *p3)
 {
     while (1) {
+        LOG_INF("T_LOW running");
         k_msleep(300);
     }
 }
 
-K_THREAD_DEFINE(thread_a, STACK_SIZE, thread_a_fn,
-                NULL, NULL, NULL, PRIO_A, 0, 0);
-K_THREAD_DEFINE(thread_b, STACK_SIZE, thread_b_fn,
-                NULL, NULL, NULL, PRIO_B, 0, 0);
+void t_med_fn(void *p1, void *p2, void *p3)
+{
+    while (1) {
+        LOG_INF("T_MED running");
+        k_msleep(200);
+    }
+}
+
+void t_high_fn(void *p1, void *p2, void *p3)
+{
+    while (1) {
+        LOG_INF("T_HIGH running");
+        k_msleep(100);
+    }
+}
+
+K_THREAD_DEFINE(thread_low, STACK_SIZE, t_low_fn,
+                NULL, NULL, NULL, PRIO_LOW, 0, 0);
+K_THREAD_DEFINE(thread_med, STACK_SIZE, t_med_fn,
+                NULL, NULL, NULL, PRIO_MED, 0, 0);
+K_THREAD_DEFINE(thread_high, STACK_SIZE, t_high_fn,
+                NULL, NULL, NULL, PRIO_HIGH, 0, 0);
+K_THREAD_DEFINE(thread_cop, STACK_SIZE, t_cop_fn,
+                NULL, NULL, NULL, PRIO_COP, 0, 0);
 
 int main(void)
 {
