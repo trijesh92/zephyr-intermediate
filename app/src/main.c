a9ee0f42bfa43725067646e06031da7205718b65
Rijesh <trijesh92@gmail.com>
2026-09-02 20:37:22 +0200

    fix: added step count in log

--- app/src/main.c
+++ app/src/main.c
@@ -10,38 +10,46 @@ LOG_MODULE_REGISTER(demo, LOG_LEVEL_DBG);
 #define PRIO_MED 5
 #define PRIO_HIGH 3
 
+#define T_PRIO_LOW_SLEEP 300
+#define T_PRIO_MED_SLEEP 200
+#define T_PRIO_HIGH_SLEEP 100
+
 void t_cop_fn(void *p1, void *p2, void *p3)
 {
-    while (1) {
-        for(uint8_t i=0;i<5;i++)
-        {
-            LOG_INF("T_COP running %d", i);
-        }
-        k_yield();
+    for(uint8_t i=0;i<5;i++)
+    {
+        LOG_INF("T_COP  step=%d tick=%d", i, k_uptime_get_32());
     }
+    k_yield();
 }
 
 void t_low_fn(void *p1, void *p2, void *p3)
 {
+    uint8_t i = 0;
     while (1) {
-        LOG_INF("T_LOW running");
-        k_msleep(300);
+        LOG_INF("T_LOW  step=%d tick=%d", i, k_uptime_get_32());
+        k_msleep(T_PRIO_LOW_SLEEP);
+        i++;
     }
 }
 
 void t_med_fn(void *p1, void *p2, void *p3)
 {
+    uint8_t i = 0;
     while (1) {
-        LOG_INF("T_MED running");
-        k_msleep(200);
+        LOG_INF("T_MED  step=%d tick=%d", i, k_uptime_get_32());
+        k_msleep(T_PRIO_MED_SLEEP);
+        i++;
     }
 }
 
 void t_high_fn(void *p1, void *p2, void *p3)
 {
+    uint8_t i = 0;
     while (1) {
-        LOG_INF("T_HIGH running");
-        k_msleep(100);
+        LOG_INF("T_HIGH step=%d tick=%d", i, k_uptime_get_32());
+        k_msleep(T_PRIO_HIGH_SLEEP);
+        i++;
     }
 }
 
@@ -56,6 +64,7 @@ K_THREAD_DEFINE(thread_cop, STACK_SIZE, t_cop_fn,
 
 int main(void)
 {
+    LOG_INF("Zephyr Thread Scheduling Demo");
     return 0;
 }
 
